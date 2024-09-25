8a6b2ae4e005be2a286bebffaf9e190e54775841
Rediet <red8alx@gmail.com>
2024-09-25 20:04:50 +0300

    Deep Dive into Django Models and Views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from . import views list_books
+from . import views
+from .views import list_books
 
 
 urlpatterns = [
     path("", views.index, name="index"), 
-    path("list_books/", views.list_books, name="list_books"),
+    path("list_books/", list_books, name="list_books"),
     path("library_detail/", views.LibraryDetailView.as_view(), name="library_detail"),
 
 ]
