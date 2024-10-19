906845aed38eb58aecd9bf414f3c706dcc7a046e
Rediet <red8alx@gmail.com>
2024-10-20 00:58:24 +0300

    Advanced API Development with Django REST Framework

---
+++ advanced-api-project/api/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+
+# Register your models here.
+from .models import Book, Author
+
+admin.site.register(Book)
+admin.site.register(Author)
